357e21883ba3d7de39a1d65bab1a473066bb5ee5
Niko <nick.oshinov@gmail.com>
2019-07-12 01:24:48 -0400

    Added additional component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HobbiesByTypeComponent } from './hobbies-by-type/hobbies-by-type.component';
+import { UsersOverTimeComponent } from './users-over-time/users-over-time.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    HobbiesByTypeComponent
+    HobbiesByTypeComponent,
+    UsersOverTimeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users-over-time/users-over-time.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-over-time',
+  templateUrl: './users-over-time.component.html',
+  styleUrls: ['./users-over-time.component.scss']
+})
+export class UsersOverTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
