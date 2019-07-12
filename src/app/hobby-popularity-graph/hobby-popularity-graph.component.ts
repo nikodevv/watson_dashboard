892b4cb07aad9daef7c892d192995975abf541a4
Niko <nick.oshinov@gmail.com>
2019-07-12 11:51:18 -0400

    Added bars for last 24 hours only

--- src/app/hobby-popularity-graph/hobby-popularity-graph.component.ts
+++ src/app/hobby-popularity-graph/hobby-popularity-graph.component.ts
@@ -15,9 +15,10 @@ export class HobbyPopularityGraphComponent implements OnInit {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  private fetchedHobbies: Array<Hobby>;
-  private hobbiesArray: Array<string> = [];
-  public data = [{ data : [] , label: 'placeholder'}];
+  public data = [
+    { data : [] , label: 'Hobbies (All Time)'},
+    { data: [], label: 'Hobbies (last 24 hours)' }
+  ];
   public chartOptions = {
     responsive: true,
     scales: {
@@ -27,18 +28,14 @@ export class HobbyPopularityGraphComponent implements OnInit {
         }
       }]
     }};
+  private fetchedHobbies: Array<Hobby>;
+  public labels: Array<string> = [];
 
   get hobbyData(): Array<Hobby> {
     return this.fetchedHobbies;
   }
   set hobbyData(hobbies) {
     this.fetchedHobbies = hobbies;
-  }
-  get labels(): Array<string> {
-    return this.hobbiesArray;
-  }
-  set labels(value) {
-    this.hobbiesArray = value;
   }
 
   constructor() { }
@@ -54,21 +51,27 @@ export class HobbyPopularityGraphComponent implements OnInit {
   organizeData() {
     const labels = [];
     const labelMap = {};
-    const occurrences = [];
+    const occurrences = []; // includes all dates
+    const occurances24Hours = []; // includes data points only created in the last 24 hours.
+    const currentTime = (new Date()).getTime();
     _.transform(this.hobbyData, (result, value) => {
       const hobby = value.value;
+      const date = +value.created_at * 1000;
+      console.log(date);
       if (labels.includes(hobby)) {
         occurrences[labelMap[hobby]] += 1;
+        occurances24Hours[labelMap[hobby]] += date + 86400000 > currentTime ? 1 : 0;
       } else {
-        console.log(hobby);
         labels.push(hobby);
-        occurrences.push(1);
         labelMap[hobby] = labels.length - 1;
+        occurrences.push(1);
+        occurances24Hours.push(date + 86400000 > currentTime ? 1 : 0);
       }
     });
 
     this.labels = labels;
-    this.data = [{ data: occurrences , label: 'placeholder'}];
+    this.data[0].data = occurrences;
+    this.data[1].data = occurances24Hours;
   }
 
   async loadData() {
